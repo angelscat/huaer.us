e536863ba8301b83f450ab60ec251cf408f61115
angelscat <angelscat@vip.qq.com>
2013-04-28 10:47:38 +0800

    install.info

---
+++ huaer_dede/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '209.200.244.80';
+$cfg_dbname = 'taoyi0_huaer';
+$cfg_dbuser = 'taoyi0_angelscat';
+$cfg_dbpwd = 'WANG1986';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
