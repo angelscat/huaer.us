eb64009c0577174dca844e6c1042fdd67b472ba8
angelscat <angelscat@vip.qq.com>
2013-04-28 11:14:18 +0800

    LOCAL.

--- huaer_dede/data/common.inc.php
+++ huaer_dede/data/common.inc.php
@@ -1,9 +1,9 @@
 <?php
 //数据库连接信息
-$cfg_dbhost = '209.200.244.80';
-$cfg_dbname = 'taoyi0_huaer';
-$cfg_dbuser = 'taoyi0_angelscat';
-$cfg_dbpwd = 'WANG1986';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'huaer_online';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = '';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
--- huaer_dede/data/config.cache.bak.php
+++ huaer_dede/data/config.cache.bak.php
@@ -1,7 +1,7 @@
 <?php
 $cfg_basehost = 'http://huaer.us';
 $cfg_cmspath = '';
-$cfg_cookie_encode = 'ZeOWg3177P';
+$cfg_cookie_encode = 'EoQXe7706P';
 $cfg_indexurl = '/';
 $cfg_backup_dir = 'backupdata';
 $cfg_indexname = '主页';
