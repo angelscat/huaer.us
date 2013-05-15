d4058d10120b70bec272774060fdeee19e0cd841
angelscat <angelscat@vip.qq.com>
2013-05-15 20:51:54 +0800

    add article catalog.

--- huaer_dede/templets/huaer/assets/js/core.js
+++ huaer_dede/templets/huaer/assets/js/core.js
@@ -71,7 +71,7 @@ jQuery.fn.makeSelector = function(){
 		selectorContainers.push(selectorContainer);
 	})
 	return selectorContainers;
-}
+};
 
 jQuery.fn.makeFileSelector = function(){
 	var selectorContainers = [];
@@ -105,7 +105,7 @@ jQuery.fn.makeFileSelector = function(){
 		selectorContainers.push(selectorContainer);
 	})
 	return selectorContainers;
-}
+};
 
 jQuery.fn.vdcodeChange = function(){
 	this.each(function(i,v){
@@ -124,4 +124,154 @@ jQuery.fn.vdcodeChange = function(){
 			})
 		}
 	})
-}
+};
+
+(function($){
+	window.CatalogSelector = function(options){
+	    $.extend(this,options);
+	    this.init();
+	}
+	CatalogSelector.prototype = {
+	    template:'<div class="catalog-dialog"> \
+	                <dl class="catalogs"> \
+	                    <dt>请选择最终分类(如：写景、叙事等)</dt> \
+	                    <dd><ul class="school" id="catalogSchool"></ul></dd> \
+	                    <dd><ul class="grade" id="catalogGrade"></ul></dd> \
+	                    <dd><ul class="type" id="catalogType"></ul></dd> \
+	                </dl> \
+	            </div>',
+	    init:function(){
+	        this.catalogData = catalogdata;
+	        this.currentSchool = this.catalogData[0];
+	        this.currentGrade = this.currentSchool.children[0];
+	        // this.currentType = this.currentGrade.children[0];
+	        this._initDom();
+	        this.updateSchool();
+	        this._initEvent();
+	    },
+	    _initDom:function(){
+	        this.el = $(this.template);
+	        this.catalog = this.el.find('.catalogs');
+	        this.catalogSchool = this.el.find('#catalogSchool');
+	        this.catalogGrade = this.el.find('#catalogGrade');
+	        this.catalogType = this.el.find('#catalogType');
+	        if($.browser.msie){
+	            this.el.append('<iframe width="100%" height="100%" frameBorder="0" src="javascript:false" style="position:absolute;top:0;left:0;z-index:1;"></iframe>');
+	        }
+	        this.el.css({
+	            left:-9999,
+	            top:-9999
+	        }).appendTo($('body'));
+
+	        this.show();
+	    },
+	    updateSchool:function(){
+	        var schools = this.catalogData,
+	            lst = ''
+	        for(var i = 0, len = schools.length; i < len; i++){
+	            lst += '<li><a data-index="'+ i +'" data-id="'+ schools[i].id +'" data-type="'+ schools[i].type +'" href="javascript:;"'+ (i === 0 ? 'class="cur"' : '') +'>'+ schools[i].name +'</a></li>';
+	        }
+	        this.catalogSchool.html(lst);
+	        this.currentSchool = schools[0]
+	        this.updateGrade();
+	    },
+	    updateGrade:function(){
+	    	this.currentGrade = null;
+	        var grades = this.currentSchool.children,
+	            lst = '';
+	        for(var i = 0, len = grades.length; i < len; i++){
+	            lst += '<li><a data-index="'+ i +'" data-id="'+ grades[i].id +'" data-type="'+ grades[i].type +'" href="javascript:;"'+ (i === 0 ? 'class="cur"' : '') +'>'+ grades[i].name +'</a></li>';
+	        }
+	        this.catalogGrade.html(lst);
+	        this.currentGrade = grades[0]
+	        this.updateType();
+	    },
+	    updateType:function(){
+	    	this.currentType = null;
+	        var types = this.currentGrade.children,
+	            lst = '';
+	        for(var i = 0, len = types.length; i < len; i++){
+	            lst += '<li><a data-index="'+ i +'" data-id="'+ types[i].id +'" data-type="'+ types[i].type +'" href="javascript:;">'+ types[i].name +'</a></li>';
+	        }
+	        this.catalogType.html(lst);
+	    },
+	    _initEvent:function(){
+	        var _this = this;
+	        this.catalogSchool.on('click','a',function(){
+	            var target = $(this),
+	                index = target.attr('data-index') * 1,
+	                id = target.attr('data-id') * 1,
+	                type = target.attr('data-type') * 1;
+	            if(target.hasClass('cur')) return;
+	            _this.catalogSchool.find('a.cur').removeClass('cur');
+	            target.addClass('cur');
+	            _this.currentSchool = _this.catalogData[index];
+	            _this.updateGrade();
+	        });
+	        this.catalogGrade.on('click','a',function(){
+	            var target = $(this),
+	                index = target.attr('data-index') * 1,
+	                id = target.attr('data-id') * 1,
+	                type = target.attr('data-type') * 1;
+	            if(target.hasClass('cur')) return;
+	            _this.catalogGrade.find('a.cur').removeClass('cur');
+	            target.addClass('cur');
+	            _this.currentGrade = _this.currentSchool.children[index];
+	            _this.updateType();
+	        });
+	        this.catalogType.on('click','a',function(){
+	            var target = $(this),
+	                index = target.attr('data-index') * 1,
+	                id = target.attr('data-id') * 1,
+	                type = target.attr('data-type') * 1;
+	            _this.catalogType.find('a.cur').removeClass('cur');
+	            target.addClass('cur');
+	            _this.currentType = _this.currentGrade.children[index];
+	        });
+
+	        this.el.on('click',function(e){
+	            e.stopPropagation();
+	        }).on('click','a',function(e){
+	            var target = $(this),
+	                index = target.attr('data-index') * 1,
+	                id = target.attr('data-id') * 1,
+	                type = target.attr('data-type') * 1;
+				if(type === 3 && _this.callback){
+					console.log(_this.currentSchool, _this.currentGrade, _this.currentType)
+					_this.callback(_this.currentSchool, _this.currentGrade, _this.currentType)
+					_this.hide();
+				}
+	        })
+
+	        this._hide = function(){
+	            _this.hide();
+	        }
+	        $(document).on('click',this._hide);
+	    },
+	    show:function(left,top){
+	        var x, y, winWidth, winHeight,
+	            isStrictMode = (document.compatMode != "BackCompat");
+	        winWidth = isStrictMode ? document.documentElement.clientWidth : document.body.clientWidth;
+	        winHeight = isStrictMode ? document.documentElement.clientHeight : document.body.clientHeight;
+	        x = left || this.left || Math.max(parseInt((winWidth - this.el.width())/2),0) + $(document).scrollLeft();
+	        y = top || this.top || Math.max(parseInt((winHeight - this.el.height())/2),0) + $(document).scrollTop();
+
+	        this.el.css({
+	            left:x,
+	            top:y,
+	            zIndex:9999 
+	        })
+	    },
+	    hide:function(){
+	        this.el.css({
+	            left:-9999,
+	            top:-9999
+	        })
+	    },
+	    remove:function(){
+	        $(document).off('click',this._hide);
+	        this.el.remove();
+	    }
+	}
+})(jQuery);
+
